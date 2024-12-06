db9816b398491760fb37a1f25b087db85c6f6a7e
Marco Barrios <github@marcobarrios.fi>
2024-12-06 18:26:32 +0100

    Create ExcelRow.ts

---
+++ src/ExcelRow.ts
@@ -0,0 +1,21 @@
+/**
+* @summary ExcelRow class
+*/
+
+class ExcelRow {
+
+  /**
+  * @summary Constructs a new row using the specified keys and values
+  * @details Values must match the `ExcelRowValue` type definition
+  * Office Scripts does not support JavaScript `Object.fromEntries` function.
+  */
+
+  constructor(keys: string[], values: ExcelRowValue[]) {
+    for (const index in keys) {
+      if (keys[index].trim().length) {
+        this[keys[index].trim()] = values[index]
+      }
+    }
+  }
+  
+}
