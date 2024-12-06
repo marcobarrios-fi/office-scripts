5f5a9c07b0d87126966a130186f7509b51db40c8
Marco Barrios <github@marcobarrios.fi>
2024-12-06 09:23:16 +0100

    Update getRangeAsArrayOfObjects.ts

--- src/getRangeAsArrayOfObjects.ts
+++ src/getRangeAsArrayOfObjects.ts
@@ -1,14 +1,15 @@
 /**
+* @requires normalizeRangeData
 * @requires getObjectFromKeysAndValues
 * @summary Converts the given Excel range to an array of objects
 * @details The values of the first row are used as the the object keys.
 * Office Scripts does not support JavaScript `Object.fromEntries` function.
-* @argument {ExcelScript.Range} range Excel range
 * @returns {array} Returns an array of objects
 */
 
-function getRangeAsArrayOfObjects(range: ExcelScript.Range) {
-  return range.getOffsetRange(1, 0).getValues().map(values =>
-    getObjectFromKeysAndValues(range.getRow(0).getTexts()[0], values)
+function getRangeAsArrayOfObjects(range: ExcelScript.Range): {}[] {
+  const keys = range.getRow(0).getTexts()[0];
+  return normalizeRangeData(range.getOffsetRange(1, 0)).map(
+    row  => getObjectFromKeysAndValues(keys, row)
   );
 }
