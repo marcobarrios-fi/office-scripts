dfb1569857a69885978f8af07ca0a443489c3885
Marco Barrios <github@marcobarrios.fi>
2024-12-06 19:24:39 +0100

    Update setRangeBorder.ts

--- src/setRangeBorder.ts
+++ src/setRangeBorder.ts
@@ -7,26 +7,20 @@
 */
 
 function setRangeBorder(range: ExcelScript.Range, borderColor: string = '#000000', borderWeight: keyof typeof ExcelScript.BorderWeight = 'medium') {
-
   const topBorder = range.getRow(0).getFormat().getRangeBorder(ExcelScript.BorderIndex.edgeTop);
   const bottomBorder = range.getRow(range.getRowCount() - 1).getFormat().getRangeBorder(ExcelScript.BorderIndex.edgeBottom);
   const leftBorder = range.getColumn(0).getFormat().getRangeBorder(ExcelScript.BorderIndex.edgeLeft);
   const rightBorder = range.getColumn(range.getColumnCount() - 1).getFormat().getRangeBorder(ExcelScript.BorderIndex.edgeRight);
-
   // Top border color and weight
   topBorder.setColor(borderColor);
   topBorder.setWeight(ExcelScript.BorderWeight[borderWeight]);
-  
   // Bottom border color and weight
   bottomBorder.setColor(borderColor);
   bottomBorder.setWeight(ExcelScript.BorderWeight[borderWeight]);
-
   // Left border color and weight
   leftBorder.setColor(borderColor);
   leftBorder.setWeight(ExcelScript.BorderWeight[borderWeight]);
-
   // Right border color and weight
   rightBorder.setColor(borderColor);
   rightBorder.setWeight(ExcelScript.BorderWeight[borderWeight]);
-  
 }
