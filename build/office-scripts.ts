189fc77ea56d118444a8b8f0210c058cec426de3
Marco Barrios <github@marcobarrios.fi>
2024-12-06 18:59:35 +0100

    Update office-scripts.ts

--- build/office-scripts.ts
+++ build/office-scripts.ts
@@ -488,42 +488,22 @@ function removeEmptyRowsFromRange(range: ExcelScript.Range) {
 * @returns {void} Returns undefined
 */
 
-function setRangeBorder(
-  range: ExcelScript.Range, 
-  borderColor: string = '#000000', 
-  borderWeight: keyof typeof ExcelScript.BorderWeight = 'medium'
-) {
+function setRangeBorder(range: ExcelScript.Range, borderColor: string = '#000000', borderWeight: keyof typeof ExcelScript.BorderWeight = 'medium') {
   // Top border weight
-  range.getRow(0).getFormat().
-    getRangeBorder(ExcelScript.BorderIndex.edgeTop).
-    setWeight(ExcelScript.BorderWeight[borderWeight]);
+  range.getRow(0).getFormat().getRangeBorder(ExcelScript.BorderIndex.edgeTop).setWeight(ExcelScript.BorderWeight[borderWeight]);
   // Bottom border weight
-  range.getRow(range.getRowCount() - 1).getFormat().
-    getRangeBorder(ExcelScript.BorderIndex.edgeBottom).
-    setWeight(ExcelScript.BorderWeight[borderWeight]);
+  range.getRow(range.getRowCount() - 1).getFormat().getRangeBorder(ExcelScript.BorderIndex.edgeBottom).setWeight(ExcelScript.BorderWeight[borderWeight]);
   // Left border weight
-  range.getColumn(0).getFormat().
-    getRangeBorder(ExcelScript.BorderIndex.edgeLeft).
-    setWeight(ExcelScript.BorderWeight[borderWeight]);
+  range.getColumn(0).getFormat().getRangeBorder(ExcelScript.BorderIndex.edgeLeft).setWeight(ExcelScript.BorderWeight[borderWeight]);
   // Right border weight
-  range.getColumn(range.getColumnCount() - 1).getFormat().
-    getRangeBorder(ExcelScript.BorderIndex.edgeRight).
-    setWeight(ExcelScript.BorderWeight[borderWeight]);
+  range.getColumn(range.getColumnCount() - 1).getFormat().getRangeBorder(ExcelScript.BorderIndex.edgeRight).setWeight(ExcelScript.BorderWeight[borderWeight]);
   // Top border color
-  range.getRow(0).getFormat().
-    getRangeBorder(ExcelScript.BorderIndex.edgeTop).
-    setColor(borderColor);
+  range.getRow(0).getFormat().getRangeBorder(ExcelScript.BorderIndex.edgeTop).setColor(borderColor);
   // Bottom border color
-  range.getRow(range.getRowCount() - 1).getFormat().
-    getRangeBorder(ExcelScript.BorderIndex.edgeBottom).
-    setColor(borderColor);
+  range.getRow(range.getRowCount() - 1).getFormat().getRangeBorder(ExcelScript.BorderIndex.edgeBottom).setColor(borderColor);
   // Left border color
-  range.getColumn(0).getFormat().
-    getRangeBorder(ExcelScript.BorderIndex.edgeLeft).
-    setColor(borderColor);
+  range.getColumn(0).getFormat().getRangeBorder(ExcelScript.BorderIndex.edgeLeft).setColor(borderColor);
   // Right border color
-  range.getColumn(range.getColumnCount() - 1).getFormat().
-    getRangeBorder(ExcelScript.BorderIndex.edgeRight).
-    setColor(borderColor);
+  range.getColumn(range.getColumnCount() - 1).getFormat().getRangeBorder(ExcelScript.BorderIndex.edgeRight).setColor(borderColor);
 }
 
