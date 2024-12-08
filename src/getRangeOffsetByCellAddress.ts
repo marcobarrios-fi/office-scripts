0d5ba0516121b6bb5c99f084cb5e9d37b7018abe
Marco Barrios <github@marcobarrios.fi>
2024-12-08 15:29:57 +0100

    Update getRangeOffsetByCellAddress.ts

--- src/getRangeOffsetByCellAddress.ts
+++ src/getRangeOffsetByCellAddress.ts
@@ -2,13 +2,9 @@
 * @requires removeEmptyRowsFromRange
 * @summary Retrieves a new range starting from the given cell address 
 * @description Throws an error if the range does not contain a cell with the given address.
-* @argument {ExcelScript.Range} range Range
-* @argument {string} cellAddress Cell address
-* @argument {boolean} [removeEmptyRows=false] A boolean value indicating whether empty rows should be removed
-* @returns {ExcelScript.Range} Returns an ExcelScript range
 */
 
-function getRangeOffsetByCellAddress(range: ExcelScript.Range, cellAddress: string, removeEmptyRows: boolean = false) {
+function getRangeOffsetByCellAddress(range: ExcelScript.Range, cellAddress: string, removeEmptyRows: boolean = false): ExcelScript.Range {
   const cell = range.getBoundingRect(cellAddress);
   if (typeof cell === 'undefined') {
     throw new Error(`Could not retrieve the cell with address ${cellAddress}.`);
