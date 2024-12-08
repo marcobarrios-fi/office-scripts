9f077cc08a61d06d9b74c0078bfb242d8aa74b9b
Marco Barrios <github@marcobarrios.fi>
2024-12-08 15:30:03 +0100

    Update getRangeOffsetByCellValue.ts

--- src/getRangeOffsetByCellValue.ts
+++ src/getRangeOffsetByCellValue.ts
@@ -1,14 +1,10 @@
 /**
-* @requires removeEmptyRowsFromRange
+* @requires getRangeOffsetByCellAddress
 * @summary Retrieves a new range starting from the cell with given value 
 * @description Throws an error if the range does not contain a cell with the given value.
-* @argument {ExcelScript.Range} range Range
-* @argument {string} cellValue Cell value
-* @argument {boolean} [removeEmptyRows=false] A boolean value indicating whether empty rows should be removed
-* @returns {ExcelScript.Range} Returns an ExcelScript range
 */
 
-function getRangeOffsetByCellValue(range: ExcelScript.Range, cellValue: string, removeEmptyRows: boolean = false) {
+function getRangeOffsetByCellValue(range: ExcelScript.Range, cellValue: string, removeEmptyRows: boolean = false): ExcelScript.Range {
   const cell = range.find(cellValue, {
     completeMatch: true,
     matchCase: true
@@ -16,21 +12,5 @@ function getRangeOffsetByCellValue(range: ExcelScript.Range, cellValue: string, 
   if (typeof cell === 'undefined') {
     throw new Error(`Could not retrieve the cell with value ${cellValue}.`);
   }
-  if (removeEmptyRows) {
-    return removeEmptyRowsFromRange(range.getOffsetRange(
-      cell.getRowIndex(),
-      cell.getColumnIndex()
-    ).getResizedRange(
-      cell.getRowIndex(),
-      -cell.getColumnIndex()
-    ));
-  } else {
-    return range.getOffsetRange(
-      cell.getRowIndex(),
-      cell.getColumnIndex()
-    ).getResizedRange(
-      cell.getRowIndex(),
-      -cell.getColumnIndex()
-    );
-  }
+  return getRangeOffsetByCellAddress(range, cell.getAddress(), removeEmptyRows);
 }
